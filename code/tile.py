99fbaed55885c9d939196d8462faae36bb80097f
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-21 22:02:49 -0300

    chore: adding test sprites

--- code/tile.py
+++ code/tile.py
@@ -5,4 +5,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
+        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
