282afb7274e972705069e55915bdf4fa34c624bb
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-21 22:03:38 -0300

    adding a player class

---
+++ code/player.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame.sprite import _Group
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, position, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = position)
