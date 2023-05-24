90da0d5abbe4536eea2f358112afe1043c1fe39a
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-24 20:59:17 -0300

    feat: implementing keyboard event movement

--- code/player.py
+++ code/player.py
@@ -6,3 +6,21 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
+
+        self.direction = pygame.math.Vector2()
+    def keyBoardInput(self):
+        keys = pygame.key.get_pressed()
+
+        if keys [pygame.K_UP]:
+            self.direction.y = -1
+        elif keys [pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys [pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys [pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
