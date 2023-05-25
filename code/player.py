9ccfbdbd4d812b2ec7e9aa6e11578929dcef5ede
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-25 00:29:46 -0300

    feat: working on collisions

--- code/player.py
+++ code/player.py
@@ -2,13 +2,15 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, position, groups):
+    def __init__(self, position, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
+
+        self.obstacle_sprites = obstacle_sprites
 
     def keyBoardInput(self):
 
@@ -31,8 +33,26 @@ class Player(pygame.sprite.Sprite):
     def movement(self, speed):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
-            
+
         self.rect.center += self.direction * speed
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = sprite.rect.bottom
+
 
     def update(self):
         self.keyBoardInput()
